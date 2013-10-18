d04b9127968cf91019730132970dd04c7168cefc
alvaro <alvaro.valles@moventos.com>
2013-10-18 17:46:16 +0200

    jugando con styles logo aplicado.

--- gen/com/faltapan/activities/R.java
+++ gen/com/faltapan/activities/R.java
@@ -10,24 +10,36 @@ package com.faltapan.activities;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int azul_oscuro=0x7f040000;
+    }
     public static final class dimen {
+        public static final int A_BTN_MARGIN=0x7f050002;
+        public static final int A_BTN_PADDING=0x7f050003;
+        public static final int LOGO_H=0x7f050009;
+        public static final int LOGO_MARGIN=0x7f05000a;
+        public static final int LOGO_W=0x7f050008;
+        public static final int TEXT_SIZE_L=0x7f050006;
+        public static final int TEXT_SIZE_M=0x7f050005;
+        public static final int TEXT_SIZE_S=0x7f050004;
+        public static final int TEXT_SIZE_XL=0x7f050007;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_falta_pan=0x7f020000;
     }
     public static final class id {
-        public static final int A_TXV_ID1=0x7f080000;
-        public static final int A_TXV_ID2=0x7f080001;
-        public static final int A_TXV_ID3=0x7f080002;
-        public static final int action_settings=0x7f080003;
+        public static final int A_TXV_ID1=0x7f090000;
+        public static final int A_TXV_ID2=0x7f090001;
+        public static final int A_TXV_ID3=0x7f090002;
+        public static final int action_settings=0x7f090003;
     }
     public static final class layout {
         public static final int layout_activity_principal=0x7f030000;
@@ -35,16 +47,17 @@ public final class R {
         public static final int layout_listado_productos=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_principal=0x7f070000;
+        public static final int activity_principal=0x7f080000;
     }
     public static final class string {
-        public static final int A_TXV_1=0x7f050002;
-        public static final int A_TXV_2=0x7f050003;
-        public static final int A_TXV_3=0x7f050004;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
+        public static final int A_TXV_1=0x7f060002;
+        public static final int A_TXV_2=0x7f060003;
+        public static final int A_TXV_3=0x7f060004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
     }
     public static final class style {
+        public static final int A_BTN_STYLE=0x7f070002;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -66,10 +79,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
